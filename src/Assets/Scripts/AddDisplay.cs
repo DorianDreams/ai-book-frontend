c236c9400fed5ffaec2446a1bb0cc6ce8111c800
Gabriel <139224059+DorianDreams@users.noreply.github.com>
2024-06-18 13:02:40 +0200

    Add progress on BookScreen

--- src/Assets/Scripts/AddDisplay.cs
+++ src/Assets/Scripts/AddDisplay.cs
@@ -10,15 +10,15 @@ public class AddDisplay : MonoBehaviour
         Debug.Log("displays connected: " + Display.displays.Length);
         // Display.displays[0] is the primary, default display and is always ON, so start at index 1.
         // Check if additional displays are available and activate each.
+        if (Display.displays.Length > 1)
+        {
+            Display.displays[0].Activate();
+            Display.displays[0].SetRenderingResolution(1080, 1920);
 
-        Display.displays[0].Activate();
-        Display.displays[0].SetRenderingResolution(1080,1920);
-
-        Display.displays[1].Activate();
-        Display.displays[1].SetRenderingResolution(1920, 1080);
-
-
-
+            Display.displays[1].Activate();
+            Display.displays[1].SetRenderingResolution(1920, 1080);
+        }
+       
     }
 
 }
