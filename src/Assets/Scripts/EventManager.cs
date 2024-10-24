2a8598db1c0b1ab84022a1546fbf70074ea1a770
Gabriel <139224059+DorianDreams@users.noreply.github.com>
2024-10-24 12:11:50 +0200

    add changes

--- src/Assets/Scripts/EventManager.cs
+++ src/Assets/Scripts/EventManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Test
 public class EventManager : MonoBehaviour
 {
     private Dictionary<string, Action<EventParam>> eventDictionary;
